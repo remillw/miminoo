7c6da9aaf9ca6d5606f0aef4ea92b2a8c4e1dfdc
remillw <140602129+remillw@users.noreply.github.com>
2025-05-27 13:40:52 +0200

    Update TextLink.vue

--- resources/js/components/TextLink.vue
+++ resources/js/components/TextLink.vue
@@ -1,19 +1,37 @@
 <script setup lang="ts">
 import { Method } from '@inertiajs/core';
 import { Link } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 interface Props {
     href: string;
     tabindex?: number;
     method?: Method;
     as?: string;
+    external?: boolean;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const isExternal = computed(() => {
+    return props.external || props.href.startsWith('http') || props.href.startsWith('mailto:') || props.href.startsWith('tel:');
+});
 </script>
 
 <template>
+    <a
+        v-if="isExternal"
+        :href="href"
+        :tabindex="tabindex"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500"
+    >
+        <slot />
+    </a>
+    
     <Link
+        v-else
         :href="href"
         :tabindex="tabindex"
         :method="method"
