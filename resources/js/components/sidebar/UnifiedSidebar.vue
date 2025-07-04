2aef39a8301e3ffb3c433916b28693f4179ad721
remillw <140602129+remillw@users.noreply.github.com>
2025-07-04 11:02:03 +0200

    Update UnifiedSidebar.vue

--- resources/js/components/sidebar/UnifiedSidebar.vue
+++ resources/js/components/sidebar/UnifiedSidebar.vue
@@ -74,7 +74,7 @@ const links = computed(() => {
 
     const babysitterLinks = [
         { name: 'Tableau de bord', href: route('dashboard', { mode: 'babysitter' }), icon: Home },
-        { name: 'Rechercher des gardes', href: route('babysitting.index'), icon: Briefcase },
+        { name: 'Rechercher des gardes', href: route('announcements'), icon: Briefcase },
         { name: 'Messagerie', href: route('messaging.index', { mode: 'babysitter' }), icon: MessageCircle },
         { name: 'Mon profil', href: route('profil', { mode: 'babysitter' }), icon: User },
         { name: 'Paiements', href: '/babysitter/paiements', icon: CreditCard },
