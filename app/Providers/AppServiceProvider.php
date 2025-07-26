56c5bbc40004c29c63364fbc894555405ace7698
remillw <140602129+remillw@users.noreply.github.com>
2025-07-26 23:08:35 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Enregistrer le canal de notification push personnalisé
-        $this->app->make(ChannelManager::class)->extend('push', function ($app) {
-            return new \App\Broadcasting\PushChannel($app->make(\App\Services\PushNotificationService::class));
+        $this->app->resolving(ChannelManager::class, function (ChannelManager $manager) {
+            $manager->extend('push', function ($app) {
+                return new \App\Broadcasting\PushChannel($app->make(\App\Services\PushNotificationService::class));
+            });
         });
     }
 }
